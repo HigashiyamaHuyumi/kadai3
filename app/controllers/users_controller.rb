f9ea0a6bf6d829476bb00c6511d444d1be95a383
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2023-09-05 16:28:42 +0000

    202309030128

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #@post_images = @user.post_images.page(params[:page])
+    @profile_images = @user.profile_images if @user.profile_images.attached?
   end
 
   def edit
