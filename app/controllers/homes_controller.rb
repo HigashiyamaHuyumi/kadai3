b20778721fb2216d9fca7404af73e082898896ef
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2023-09-18 08:02:26 +0000

    2023090181701

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,4 @@
 class HomesController < ApplicationController
-  before_action :authenticate_user!, only: [:about]
 
   def top
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get '/home/about', to: 'homes#about', as: 'home_about' #aboutページルート
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/home/about', to: 'homes#about', as: 'home_about' #aboutページルート
+
   root to: 'homes#top'
   get '/users/:id', to: 'users#show', as: 'user' #ログイン後のページルート(投稿一覧)
   get '/books', to: 'books#books', as: 'books' #全ての投稿一覧のページルート
